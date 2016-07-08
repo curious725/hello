9847fb9bc2108642da03785c3ed73d7dea3503a0
curious725 <curious725@gmail.com>
2016-07-08 11:17:42 +0300

    fixed some syntax errors

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
@@ -6,4 +6,4 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000);
-console.log('express app started on port 3000')
+console.log('express app started on port 3000');
